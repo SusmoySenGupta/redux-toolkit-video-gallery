576ddd6841f94448727f3c84efaa29c65c1d184c
SusmoySenGupta <susmoy.cse@gmail.com>
2023-03-05 14:08:04 +0600

    feat: setup routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 import Video from './pages/Video';
-// import Home from './pages/Home';
 
 function App() {
-	return <Video />;
+	return (
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/videos/:videoID" element={<Video />}></Route>
+			</Routes>
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,13 @@
-import Footer from '../components/Footer';
 import VideoGrid from '../components/grid/VideoGrid';
-import Navbar from '../components/Navbar';
 import TagList from '../components/tags/TagList';
 import Pagination from '../components/ui/Pagination';
 
 export default function Home() {
 	return (
 		<>
-			<Navbar />
-
 			<TagList />
-
 			<VideoGrid />
-
 			<Pagination />
-
-			<Footer />
 		</>
 	);
 }
